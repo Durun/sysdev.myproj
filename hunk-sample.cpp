06eb96edc5dc6de5aba04c9698b8149410a29502
long-long-float <niinikazuki@yahoo.co.jp>
2020-11-06 15:31:20 +0900

    Remove int3 and illegal access

--- hunk-sample.cpp
+++ hunk-sample.cpp
@@ -8,11 +8,8 @@ int main(int argc, char** argv) {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "input = " << input << std::endl;
 
-    asm("int3");
-
     int array[2] = {1,2};
     array[1] = 3;
-    std::cout << array[2] << std::endl;
 
     return 0;
 }
